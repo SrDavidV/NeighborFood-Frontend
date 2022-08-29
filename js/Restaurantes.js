6ab25a346f021ee9e0342c64fc7c6241df24ebab
SrDavidV <davidtique058@gmail.com>
2022-08-29 11:09:21 -0500

    Se modifican los restaurantes

--- js/Restaurantes.js
+++ js/Restaurantes.js
@@ -7,16 +7,16 @@ axios.request(options1).then(function (response) {
 
     restaurantes.forEach(resta => {
         res += `
-        <li class="slider-two__element">
-            <a href="#">
-                <img src="${resta.restA_Imagen}" />
-                <div class="slider-two__info">
-                    <h3 class="slider-two__subtitle">${resta.restA_Nombre}</h3>
-                    <span class="slider-two__address">${resta.sedes[0].seD_Direccion}</span>
-                </div>
-            </a>
-        </li>
-        `
+    <li class="slider-two__element">
+      <a href="#">
+        <img src="${resta.restA_Imagen}" />
+        <div class="slider-two__info">
+          <h3 class="slider-two__subtitle">${resta.restA_Nombre}</h3>
+          <span class="slider-two__address">${resta.sedes[0].seD_Direccion}</span>
+        </div>
+      </a>
+    </li>
+    `
 
         document.getElementById('highlights-slider').innerHTML = res;
     });
